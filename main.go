478f6eccbbfc400aa5703020b0a009408adf09eb
Drew Ripberger <drew.ripberger@gmail.com>
2019-02-28 22:20:38 -0500

    finished testing procedure + sample test data for n=20

--- main.go
+++ main.go
@@ -1,12 +1,103 @@
 package main
 
 import(
-	//"fmt"
+	"fmt"
 	"time"
+	"runtime"
 	"github.com/drewrip/availsim/raft"
+	"github.com/fatih/color"
 )
 
+type Data struct{
+	Size		int
+
+	//In Milliseconds
+	TotalTime	int64
+
+	Trials		int
+}
+
 func main(){
-	raft.NewCluster(3)
-	time.Sleep(10 * time.Second)
+	runtime.GOMAXPROCS(4)
+	TRIALS:=200
+	data:=make([]Data,0)
+	dinghydata:=make([]Data,0)
+	// Making Data points
+	for i:=3; i<53; i+=2{
+		data = append(data, Data{Size: i, TotalTime: 0, Trials: 0})
+		dinghydata = append(dinghydata, Data{Size: i, TotalTime: 0, Trials: 0})
+	}
+	for x:=0; x<TRIALS; x++{
+		ind:=0
+		for i:=3; i<53; i+=2{
+			color.Yellow("[TEST] Starting for n=%d\n", i)
+			leaderChange := make(chan raft.ElectionTimePair, 2048)
+			_,k := raft.NewCluster(i, 10 * time.Millisecond, leaderChange, false)
+			time.Sleep(1000 * time.Millisecond)
+
+			// Need to consume the change message sent during initialization
+			<-leaderChange
+
+			k<-struct{}{}
+			start:=time.Now()
+			var elTime raft.ElectionTimePair
+			select{
+			case elTime=<-leaderChange:
+			case<-time.After(1 * time.Second):
+				i-=2
+				continue
+			}
+			end:=time.Now()
+			totalEl:=end.Sub(start)
+			el:=int64(elTime.End.Sub(elTime.Start))
+			color.Red("Total Elapsed Time: %dns\n", totalEl)
+			color.Red("Took: %dns\n", el)
+			data[ind].TotalTime += int64(totalEl)
+			data[ind].Trials++
+			ind++
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+
+	for x:=0; x<TRIALS; x++{
+		ind:=0
+		for i:=3; i<53; i+=2{
+			color.Yellow("[TEST] Starting for n=%d\n", i)
+			leaderChange := make(chan raft.ElectionTimePair, 2048)
+			_,k := raft.NewCluster(i, 10 * time.Millisecond, leaderChange, true)
+			time.Sleep(1000 * time.Millisecond)
+
+			// Need to consume the change message sent during initialization
+			<-leaderChange
+
+			k<-struct{}{}
+			start:=time.Now()
+			var elTime raft.ElectionTimePair
+			select{
+			case elTime=<-leaderChange:
+			case<-time.After(1 * time.Second):
+				i-=2
+				continue
+			}
+			end:=time.Now()
+			totalEl:=end.Sub(start)
+			el:=int64(elTime.End.Sub(elTime.Start))
+			color.Red("Total Elapsed Time: %dns\n", totalEl)
+			color.Red("Took: %dns\n", el)
+			dinghydata[ind].TotalTime += int64(totalEl)
+			dinghydata[ind].Trials++
+			ind++
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+
+	fmt.Printf("Vanilla Raft (Total Time):\n")
+	for i:=0; i<len(data); i++{
+		fmt.Printf("%d\t%d\n", data[i].Size, data[i].TotalTime/int64(data[i].Trials))
+	}
+
+	fmt.Printf("Dinghy Raft (Total Time):\n")
+	for i:=0; i<len(dinghydata); i++{
+		fmt.Printf("%d\t%d\n", dinghydata[i].Size, dinghydata[i].TotalTime/int64(dinghydata[i].Trials))
+	}
 }
--- raft/raft.go
+++ raft/raft.go
@@ -8,7 +8,6 @@ import(
 
 const(
 	defaultTimeout time.Duration = 500*time.Millisecond
-	rpcLatency time.Duration = 10 * time.Millisecond
 )
 
 type RaftState byte
@@ -30,8 +29,9 @@ type Ping struct {
 	From string
 	Recv bool
 	Term int
+	Install bool
+	NewTimeout time.Duration
 }
-
 
 type RequestVoteRequest struct {
 	// Arguments
@@ -89,6 +89,11 @@ func (v RequestVoteResponse) GetTerm() int {
 	return v.Term
 }
 
+type ElectionTimePair struct {
+	Start time.Time
+	End time.Time
+}
+
 // Main structure for Raft, holds and represents a Raft node
 type Raft struct {
 	HeartbeatTimeout time.Duration
@@ -99,17 +104,20 @@ type Raft struct {
 	VotedFor string
 	State RaftState
 	QuorumSize int
+	KillChan chan struct{}
+	Latency time.Duration
+	LeaderAlert chan ElectionTimePair
+	ETP ElectionTimePair
 }
 
 // NewCluster takes in the size of the cluster to create and returns references to the nodes
-func NewCluster(z int) []*Raft {
+func NewCluster(z int, lat time.Duration, la chan ElectionTimePair, ping bool) ([]*Raft, chan struct{}) {
 	c := make([]*Raft, 0)
-
 	peers := make(map[string](RPCChan))
-
+	var leaderKillChan chan struct{}
 	// Creating the connections between each of the nodes
 	for i:=0; i<z; i++{
-		peers[fmt.Sprintf("n%d", i)] = RPCChan(make(chan RPC, 1028))
+		peers[fmt.Sprintf("n%d", i)] = RPCChan(make(chan RPC, 2048))
 	}
 
 	// Creating the actuall Raft structs
@@ -124,8 +132,12 @@ func NewCluster(z int) []*Raft {
 		delete(myPeers, fmt.Sprintf("n%d", i))
 
 		stateForRaft := Follower
+		var myKillChan chan struct{}
+		myKillChan = nil
 		if i == 0{
 			stateForRaft = Leader
+			myKillChan = make(chan struct{}, 2048)
+			leaderKillChan = myKillChan
 		}
 		r := &Raft{
 			HeartbeatTimeout:	defaultTimeout,
@@ -136,19 +148,22 @@ func NewCluster(z int) []*Raft {
 			VotedFor:			"",
 			State:				stateForRaft,
 			QuorumSize:			(z/2)+1,
+			KillChan:			myKillChan,
+			Latency:			lat,
+			LeaderAlert:		la,
+			ETP:				ElectionTimePair{},
 		}
 		c = append(c, r)
 	}
-	time.Sleep(1 * time.Second)
-	StartLoops(c)
-	return c
+	time.Sleep(500 * time.Millisecond)
+	StartLoops(c, ping)
+	return c, leaderKillChan
 }
 
-func StartLoops(rs []*Raft){
-	fmt.Println(rs)
+func StartLoops(rs []*Raft, ping bool){
 	for _,n := range rs{
 		fmt.Printf("[INIT] Staring Raft Loop for %s\n", n.ID)
-		go n.RaftHandler()
+		go n.RaftHandler(ping)
 	}
 }
 
@@ -157,23 +172,47 @@ func randTimeout(random *rand.Rand, min time.Duration) time.Duration {
 }
 
 // This is the loop to handle raft's actions and different RPC communication
-func (r *Raft) RaftHandler(){
+func (r *Raft) RaftHandler(ping bool){
     random := rand.New(rand.NewSource(time.Now().UnixNano()))
-    killTime:=time.After(3 * time.Second)
 	for{
 		switch r.State{
 
 		case Leader:
+			r.LeaderAlert<-r.ETP
 			fmt.Printf("[STATE] %s: Now a Leader\n", r.ID)
 			/*for k := range r.Peers{
 				makeRPC(r.Peers[k], Ping{From: r.ID, Recv: false})
 			}
 			*/
+			if ping{
+				start := time.Now()
+				for k := range r.Peers{
+					makeRPC(r.Peers[k], Ping{From: r.ID, Install: false})
+				}
+				resps:=0
+				for resps < len(r.Peers){
+					x:=<-r.RecvChan
+					_, ok := x.(Ping)
+					if !ok{
+						continue
+					}
+					resps++
+				}
+				el:=time.Since(start)
+
+				for k := range r.Peers{
+					makeRPC(r.Peers[k], Ping{From: r.ID, Install: false, NewTimeout: el*20})
+				}
+
+				fmt.Printf("[INIT] timeout established at %v\n", el*20)
+			}
+
 			ticker := time.NewTicker(r.HeartbeatTimeout/10).C
 			LeaderLoop:
 			for{
 				select{
 				case x:=<-r.RecvChan:
+					time.Sleep(r.Latency)
 					switch x.(type){
 						case RequestVoteRequest:
 							if x.GetTerm() > r.Term{
@@ -188,13 +227,16 @@ func (r *Raft) RaftHandler(){
 								r.State = Follower
 								break LeaderLoop
 							}
+
+
 					}
 				case <-ticker:
 					for k := range r.Peers{
 						makeRPC(r.Peers[k], AppendEntries{From: r.ID, Term: r.Term, LeaderID: r.ID})
 					} 
 
-				case <-killTime:
+				case <-r.KillChan:
+					fmt.Printf("[DEBUG] %s: KILL\n", r.ID)
 					return
 				}
 			}
@@ -203,6 +245,7 @@ func (r *Raft) RaftHandler(){
 			fmt.Printf("[STATE] %s: Now a Follower\n", r.ID)
 			FollowerLoop:
 			for{
+				time.Sleep(r.Latency)
 				select{
 					// Determines how to handle incoming RPC
 					case m:=<-r.RecvChan:
@@ -210,8 +253,12 @@ func (r *Raft) RaftHandler(){
 
 						// Handles Ping requests from leader
 						case Ping:
-							makeRPC(r.Peers[m.Sender()], Ping{From: r.ID, Recv: true})
-
+							m, _ := m.(Ping)
+							if m.Install{
+								r.HeartbeatTimeout = m.NewTimeout
+							} else {
+								makeRPC(r.Peers[m.Sender()], Ping{From: r.ID, Recv: true})
+							}
 						// Handles Vote Requests from candidate
 						case RequestVoteRequest:
 							fmt.Printf("[RPC] %s: Received RequestVoteRequest\n", r.ID)
@@ -219,6 +266,7 @@ func (r *Raft) RaftHandler(){
 								makeRPC(r.Peers[m.Sender()], RequestVoteResponse{VoteGranted: true, Term: r.Term})
 								r.Term = m.GetTerm()
 								r.VotedFor = m.Sender()
+								goto FollowerLoop
 							} else {
 								makeRPC(r.Peers[m.Sender()], RequestVoteResponse{VoteGranted: false, Term: r.Term})
 							}
@@ -235,6 +283,7 @@ func (r *Raft) RaftHandler(){
 					case <-time.After(randTimeout(random, r.HeartbeatTimeout)):
 						fmt.Printf("%s: heartbeat timeout\n", r.ID)
 						r.State = Candidate
+						r.ETP.Start = time.Now()
 						break FollowerLoop
 
 					
@@ -256,27 +305,34 @@ func (r *Raft) RaftHandler(){
 			for{
 				select{
 				case <-time.After(randTimeout(random, r.HeartbeatTimeout)):
-					r.State = Follower
 					break CandidateLoop
 				case x:=<-r.RecvChan:
-					fmt.Println(x)
-					ballot, ok := x.(RequestVoteResponse)
-					if !ok{
-						fmt.Printf("[ERR] I don't understand this RPC\n")
-						break CandidateLoop
+					time.Sleep(r.Latency)
+					switch x.(type){
+						case RequestVoteResponse:
+							fmt.Printf("%s: Received RequestVoteResponse\n", r.ID)
+							ballot, ok := x.(RequestVoteResponse)
+							if !ok{
+								fmt.Printf("[ERR] I don't understand this RPC\n")
+							}
+							if ballot.GetTerm() > r.Term{
+								r.State = Follower
+								break CandidateLoop
+							}
+							if ballot.VoteGranted{
+								votes++
+							}
+							if votes>=r.QuorumSize{
+								r.State = Leader
+								r.ETP.End = time.Now()
+								break CandidateLoop
+							}
+						case AppendEntries:
+							fmt.Printf("[STATE] Candidate %s: received AppendEntries stepping down\n", r.ID)
+							r.State = Follower
+							break CandidateLoop
 					}
-					fmt.Printf("%s: Received RequestVoteResponse\n", r.ID)
-					if ballot.GetTerm() > r.Term{
-						r.State = Follower
-						break CandidateLoop
-					}
-					if ballot.VoteGranted{
-						votes++
-					}
-					if votes>=r.QuorumSize{
-						r.State = Leader
-						break CandidateLoop
-					}
+
 					
 				}
 				fmt.Printf("%s: Votes=%d\n", r.ID, votes)
